df0cb4012bb4a731795e48e601a203dfbea1bdb2
austinpeng <18723558607Sj>
2020-12-16 10:05:32 +0800

    注释

--- src/main/java/com/austin/custom/spring/utils/TransactionManager.java
+++ src/main/java/com/austin/custom/spring/utils/TransactionManager.java
@@ -16,14 +16,23 @@ public class TransactionManager {
         return instance;
     }
 
+    /**
+     * 开启事务
+     **/
     public void beginTransaction() throws SQLException {
         ConnectionUtils.getInstance().getCurrentConnect().setAutoCommit(false);
     }
 
+    /**
+     * 提交事务
+     **/
     public void commit() throws SQLException {
         ConnectionUtils.getInstance().getCurrentConnect().commit();
     }
 
+    /**
+     * 回滚事务
+     **/
     public void rollback() throws SQLException {
         ConnectionUtils.getInstance().getCurrentConnect().rollback();
     }
